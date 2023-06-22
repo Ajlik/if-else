600a4693085ecfcd444aa8ca3eea5aa20ed14b20
Ajlik <122724168+Ajlik@users.noreply.github.com>
2023-06-22 22:02:05 +0300

    Add files via upload

---
+++ что то/что то/что то.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "RU");
+
+	int op;
+
+	cout << "введите число \n";
+	cin >> op;
+	
+	if (op < 10)
+		cout << "это число меньше 10";
+	else if(op = 10)
+		cout << "это число 10"; 
+	else if (op > 10)
+		cout << "это число больше 10";
+
+
+
+
+	return 0;
+}
+
